ea2cb507f482c1afd549da7b9737f3f523b4e308
Gurpreet Singh <63188787+preetchani@users.noreply.github.com>
2021-10-20 20:21:57 +0530

    Navbar component created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import {Link,Route,Switch} from 'react-router-dom';
 import {Layout,Typography,Space} from 'antd';
+import { Navbar } from './components/Navbar';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <div className="navbar"></div>
+      <div className="navbar"><Navbar/></div>
       <div className="main"></div>
       <div className="footer"></div>
     </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,31 @@
 import React from 'react';
 import { Button,Typography,Menu,Avatar } from 'antd';
 import { Link } from 'react-router-dom';
-import {HomeOutlined,MoneyOutlined,BulbOutlined,FundOutlined} from '@ant-design/icons';
-export const navbar = () => {
+import {HomeOutlined,MoneyCollectOutlined,BulbOutlined,FundOutlined} from '@ant-design/icons';
+import icon from '../images/cryptocurrency.png'
+export const Navbar = () => {
     return (
         <div className="nav-container">
             <div className="logo-container">
-                <Avatar/>
+                <Avatar src={icon} size='large'/>
                 <Typography.Title level={2} className="logo">
                     <Link to="/">Cryptoly</Link>
                 </Typography.Title>
             </div>
+            <Menu theme="dark">
+                <Menu.Item icon={<HomeOutlined/>}>
+                    <Link to="/">Home</Link>
+                </Menu.Item>
+                <Menu.Item icon={<FundOutlined/>}>
+                    <Link to="/crytocurrencies">cryptocurrencies</Link>
+                </Menu.Item>
+                <Menu.Item icon={<MoneyCollectOutlined/>}>
+                    <Link to="/exchanges">Exchanges</Link>
+                </Menu.Item>
+                <Menu.Item icon={<BulbOutlined/>}>
+                    <Link to="/news">News</Link>
+                </Menu.Item>
+            </Menu>
         </div>
     )
 }

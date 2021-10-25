3a5e5b4d3b5c175692f85db3abb919e3c99ce850
preetchani <preetchani23498@gmail.com>
2021-10-25 15:48:56 +0530

    Add conditional rendering for search bar

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -20,27 +20,43 @@ const Cryptocurrencies = ({limited}) => {
     console.log(cryptos);   
     if(isFetching) return "Loading..."
     return (
-        <>
-            <div className="search-crypto">
-                <Input placeholder="Search CryptoCurrency" onChange={(e)=>setSearchTerm(e.target.value)}/>
-            </div>
-            <Row gutter={[32,32]} className="crypto-card-container">
-                {
-                    cryptos?.map((currency)=>(
-                        <Col xs={24} sm={12} lg={6} className="crypto-card" key={currency.id}>
-                            <Link to={`./crypto/${currency.id}`}>
-                                <Card title={`${currency.rank}. ${currency.name}`} extra={<img className="crypto-image" src={currency.iconUrl}/>} hoverable>
-                                    <p>Price: {millify(currency.price)}</p>
-                                    <p>Market Cap: {millify(currency.marketCap)}</p>
-                                    <p>Daily Change: {millify(currency.change)}%</p>
-                                </Card>
-                            </Link>
-                        </Col>
-                    ))
-                }
-            </Row>
-        </>
-    )
+      <>
+        {!limited && (
+          <div className="search-crypto">
+            <Input
+              placeholder="Search CryptoCurrency"
+              onChange={(e) => setSearchTerm(e.target.value)}
+            />
+          </div>
+        )}
+
+        <Row gutter={[32, 32]} className="crypto-card-container">
+          {cryptos?.map((currency) => (
+            <Col
+              xs={24}
+              sm={12}
+              lg={6}
+              className="crypto-card"
+              key={currency.id}
+            >
+              <Link to={`./crypto/${currency.id}`}>
+                <Card
+                  title={`${currency.rank}. ${currency.name}`}
+                  extra={
+                    <img className="crypto-image" alt="icon" src={currency.iconUrl} />
+                  }
+                  hoverable
+                >
+                  <p>Price: {millify(currency.price)}</p>
+                  <p>Market Cap: {millify(currency.marketCap)}</p>
+                  <p>Daily Change: {millify(currency.change)}%</p>
+                </Card>
+              </Link>
+            </Col>
+          ))}
+        </Row>
+      </>
+    );
 }
 
 export default Cryptocurrencies;

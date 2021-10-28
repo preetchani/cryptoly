e17fc94c4eafb3bf72198da788eba21acc1f5db8
preetchani <preetchani23498@gmail.com>
2021-10-28 20:45:04 +0530

    Code format

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,62 +1,83 @@
-import React, { useState } from 'react';
-import { useGetCryptoNewsDataQuery } from '../Services/cryptoNews';
-import { useGetCryptoDataQuery } from '../Services/cryptoApi';
-import moment from 'moment';
-import {Row,Col,Select,Typography,Card,Avatar} from 'antd';
+import React, { useState } from "react";
+import { useGetCryptoNewsDataQuery } from "../Services/cryptoNews";
+import { useGetCryptoDataQuery } from "../Services/cryptoApi";
+import moment from "moment";
+import { Row, Col, Select, Typography, Card, Avatar } from "antd";
 
-const fallbackImg='https://www.bing.com/th?id=OVFT.mpzuVZnv8dwIMRfQGPbOPC&pid=News'
-const News = ({limited}) => {
-    const [newsCategory,setNewsCategory] = useState('Cryptocurrency');
-    const {data}=useGetCryptoDataQuery(100);
-    const {data:cryptoNews} = useGetCryptoNewsDataQuery({newsCategory,count:limited?6:12});
-    console.log(cryptoNews);
-    if(!cryptoNews?.value) return "Loading..."
-    return (
-        <Row gutter={[24,24]}>
-
-            {
-                !limited && (
-                        <Col span={24}>
-                            <Select
-                                showSearch
-                                className="select-news"
-                                placeholder="select a crypto"
-                                optionFilterProp="children"
-                                onChange={(value)=>setNewsCategory(value)}
-                                filterOption={(input,option)=>option.children.toLowerCase().indexOf(input.toLowerCase())>0}
-                            >
-                                <Select.Option value="Cryptocurrency">Cryptocurrency</Select.Option>
-                                {data?.data?.coins.map((coin)=><Select.Option value={coin.name}>{coin.name}</Select.Option>)}
-                            </Select>
-                        </Col>
-                    ) 
+const fallbackImg =
+  "https://www.bing.com/th?id=OVFT.mpzuVZnv8dwIMRfQGPbOPC&pid=News";
+const News = ({ limited }) => {
+  const [newsCategory, setNewsCategory] = useState("Cryptocurrency");
+  const { data } = useGetCryptoDataQuery(100);
+  const { data: cryptoNews } = useGetCryptoNewsDataQuery({
+    newsCategory,
+    count: limited ? 6 : 12,
+  });
+  console.log(cryptoNews);
+  if (!cryptoNews?.value) return "Loading...";
+  return (
+    <Row gutter={[24, 24]}>
+      {!limited && (
+        <Col span={24}>
+          <Select
+            showSearch
+            className="select-news"
+            placeholder="select a crypto"
+            optionFilterProp="children"
+            onChange={(value) => setNewsCategory(value)}
+            filterOption={(input, option) =>
+              option.children.toLowerCase().indexOf(input.toLowerCase()) > 0
             }
-            {cryptoNews.value.map((news,i)=>(
-                <Col xs={24} sm={12} lg={8} key={i}>
-                    <Card className="news-card" hoverable>
-                        <a href={news.url} target="_blank" rel="noreferrer">
-                            <div className="news-image-container">
-                                <Typography.Title className="news-title" level={4}>
-                                    {news.name}
-                                </Typography.Title>
-                                <img style={{maxWidth:'200px',maxHeight:'100px'}} src={news?.image?.thumbnail?.contentUrl || fallbackImg} alt="icon" />
-                            </div>
-                            <p>
-                                {news.description >100 ? `${news.description.subString(0,100)}...`:news.description }
-                            </p>
-                            <div className="provider-container">
-                                <div>
-                                    <Avatar src={news.provider[0]?.image?.thumbnail?.contentUrl || fallbackImg} alt="Image" />
-                                    <Typography.Text className="provider-name">{news.provider[0]?.news}</Typography.Text>
-                                </div>
-                                <Typography.Text>{moment(news.datePublished).startOf('ss').fromNow()}</Typography.Text>
-                            </div>
-                        </a>
-                    </Card>
-                </Col>
+          >
+            <Select.Option value="Cryptocurrency">Cryptocurrency</Select.Option>
+            {data?.data?.coins.map((coin) => (
+              <Select.Option value={coin.name}>{coin.name}</Select.Option>
             ))}
-        </Row>
-    )
-}
+          </Select>
+        </Col>
+      )}
+      {cryptoNews.value.map((news, i) => (
+        <Col xs={24} sm={12} lg={8} key={i}>
+          <Card className="news-card" hoverable>
+            <a href={news.url} target="_blank" rel="noreferrer">
+              <div className="news-image-container">
+                <Typography.Title className="news-title" level={4}>
+                  {news.name}
+                </Typography.Title>
+                <img
+                  style={{ maxWidth: "200px", maxHeight: "100px" }}
+                  src={news?.image?.thumbnail?.contentUrl || fallbackImg}
+                  alt="icon"
+                />
+              </div>
+              <p>
+                {news.description > 100
+                  ? `${news.description.subString(0, 100)}...`
+                  : news.description}
+              </p>
+              <div className="provider-container">
+                <div>
+                  <Avatar
+                    src={
+                      news.provider[0]?.image?.thumbnail?.contentUrl ||
+                      fallbackImg
+                    }
+                    alt="Image"
+                  />
+                  <Typography.Text className="provider-name">
+                    {news.provider[0]?.news}
+                  </Typography.Text>
+                </div>
+                <Typography.Text>
+                  {moment(news.datePublished).startOf("ss").fromNow()}
+                </Typography.Text>
+              </div>
+            </a>
+          </Card>
+        </Col>
+      ))}
+    </Row>
+  );
+};
 
 export default News;
